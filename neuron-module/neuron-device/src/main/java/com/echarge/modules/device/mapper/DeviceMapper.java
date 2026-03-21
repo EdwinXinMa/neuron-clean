be8931019d13df424c0beaa28161d907e75cf264
Edwin <maxinstruggle@163.com>
2026-03-22 00:50:44 +0800

    fix: 删除旧 Device 实体及相关文件

--- neuron-module/neuron-device/src/main/java/com/echarge/modules/device/mapper/DeviceMapper.java
+++
@@ -1,7 +0,0 @@
-package com.echarge.modules.device.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.echarge.modules.device.entity.Device;
-
-public interface DeviceMapper extends BaseMapper<Device> {
-}
