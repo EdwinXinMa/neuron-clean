468d2bcb3e4ec498ca29a45d24a2791d4e7622dc
Edwin <maxinstruggle@163.com>
2026-03-22 00:40:25 +0800

    chore: 删除 neuron-data 模块

--- neuron-module/neuron-data/src/main/java/com/echarge/modules/data/service/IAlarmRecordService.java
+++
@@ -1,7 +0,0 @@
-package com.echarge.modules.data.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.echarge.modules.data.entity.AlarmRecord;
-
-public interface IAlarmRecordService extends IService<AlarmRecord> {
-}
