c0d8ea7452932a79fc7122d9a8a97abe8afabfe8
Edwin <maxinstruggle@163.com>
2026-03-22 00:33:48 +0800

    chore: 删除 neuron-system-biz 旧模块

--- neuron-system/neuron-system-biz/src/main/java/com/echarge/modules/openapi/mapper/OpenApiMapper.java
+++
@@ -1,9 +0,0 @@
-package com.echarge.modules.openapi.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-import com.echarge.modules.openapi.entity.OpenApi;
-
-@Mapper
-public interface OpenApiMapper extends BaseMapper<OpenApi> {
-}
--- neuron-system/neuron-system-biz/src/main/java/com/echarge/modules/openapi/service/OpenApiService.java
+++
@@ -1,8 +0,0 @@
-package com.echarge.modules.openapi.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.echarge.modules.openapi.entity.OpenApi;
-
-public interface OpenApiService extends IService<OpenApi> {
-    OpenApi findByPath(String path);
-}
