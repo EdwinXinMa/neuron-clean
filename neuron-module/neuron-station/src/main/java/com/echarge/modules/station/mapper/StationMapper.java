347d4fb34526b92b0dec46db13630b0ac5eb17db
Edwin <maxinstruggle@163.com>
2026-03-22 00:38:59 +0800

    chore: 删除 neuron-station 模块

--- neuron-module/neuron-station/src/main/java/com/echarge/modules/station/mapper/StationMapper.java
+++
@@ -1,7 +0,0 @@
-package com.echarge.modules.station.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.echarge.modules.station.entity.Station;
-
-public interface StationMapper extends BaseMapper<Station> {
-}
