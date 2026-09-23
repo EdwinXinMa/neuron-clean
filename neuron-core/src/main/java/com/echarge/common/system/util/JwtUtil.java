247ba3b8ce62cdadb393761781922366afd22202
Edwin <maxin@alwayscontrol.com.cn>
2026-09-23 08:48:27 +0800

    fix: extend app token lifetime to 180 days

--- neuron-core/src/main/java/com/echarge/common/system/util/JwtUtil.java
+++ neuron-core/src/main/java/com/echarge/common/system/util/JwtUtil.java
@@ -43,8 +43,8 @@ public class JwtUtil {
 
 	/**PC端，Token有效期为7天（Token在reids中缓存时间为两倍）*/
 	public static final long EXPIRE_TIME = (7 * 12) * 60 * 60 * 1000L;
-	/**APP端，Token有效期为30天（Token在reids中缓存时间为两倍）*/
-	public static final long APP_EXPIRE_TIME = (30 * 12) * 60 * 60 * 1000L;
+	/**APP端，Token有效期为180天（Token在reids中缓存时间为两倍）*/
+	public static final long APP_EXPIRE_TIME = 180L * 24 * 60 * 60 * 1000;
 	static final String WELL_NUMBER = SymbolConstant.WELL_NUMBER + SymbolConstant.LEFT_CURLY_BRACKET;
 
     /**
